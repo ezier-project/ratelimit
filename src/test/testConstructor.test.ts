bad5a881d63f721e35d325fcce05f55eaeac7118
Shadofer <shadowrlrs@gmail.com>
2022-04-06 12:41:32 +0300

    Format code.

--- src/test/testConstructor.test.ts
+++ src/test/testConstructor.test.ts
@@ -3,25 +3,24 @@ import { EzRateLimiter } from 'main/index';
 function testConstructorMaxPoints(): void {
     new EzRateLimiter({
         clearDelay: 1000,
-        maxPoints: -1
+        maxPoints: -1,
     });
 }
 
 function testConstructorClearDelay(): void {
     new EzRateLimiter({
         clearDelay: -1,
-        maxPoints: 10
+        maxPoints: 10,
     });
 }
 
-
-it('test constructor', (done) => {
+it('test constructor', done => {
     try {
         testConstructorMaxPoints();
-    } catch(e) {
+    } catch (e) {
         try {
             testConstructorClearDelay();
-        } catch(e) {
+        } catch (e) {
             done();
         }
     }
--- src/test/testStates.test.ts
+++ src/test/testStates.test.ts
@@ -1,14 +1,11 @@
 import { EzRateLimiter } from 'main/index';
 
-it('test states', (done) => {
+it('test states', done => {
     const ezLimiter = new EzRateLimiter();
 
-    ezLimiter.start()
-    .catch(() => {
-        ezLimiter.stop()
-        .then(() => {
-            ezLimiter.stop()
-            .catch(() => {
+    ezLimiter.start().catch(() => {
+        ezLimiter.stop().then(() => {
+            ezLimiter.stop().catch(() => {
                 done();
             });
         });
